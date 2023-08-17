354c10c3c757310e4b3811a01fe93770bee10d56
Stephan Robotta <stephan.robotta@bfh.ch>
2023-08-17 20:43:13 +0200

    add missing language key for plugin list in help menu of the tiny editor

--- lang/en/tiny_fontcolor.php
+++ lang/en/tiny_fontcolor.php
@@ -51,6 +51,7 @@ $string['textcolors'] = 'Available text colours';
 $string['textcolors_desc'] = 'A list of colour names on the left and their RGB colour code, e.g. "#000000". The values are the colours displayed in the dialogue to choose from. The names serve as as the tooltip when hovering over the colours.';
 $string['placeholdercolorname'] = 'Descriptive name of colour';
 $string['placeholdercolorvalue'] = 'Hex code of colour value';
+$string['helplinktext'] = 'Tiny font colour';
 
 // PRIVACY.
 $string['privacy:metadata'] = 'The Tiny font color plugin does not store any personal data about any user.';
